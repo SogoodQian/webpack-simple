1d5ea40c5a4d0acdd3d968ab25dd3d4064af49cd
amao <1226927180@qq.com>
2020-04-15 10:58:01 +0800

    增加配置文件

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+// console.log("--------打印配置---------");
+// console.log(path);
+// console.log(__dirname);
+// console.log(__filename);
+// console.log("--------打印配置完毕---------")
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
+
+// 课间休息中
