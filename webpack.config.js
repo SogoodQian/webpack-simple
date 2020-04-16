8e8c92511420004d1e435be740901c314a21e659
amao <1226927180@qq.com>
2020-04-16 10:37:53 +0800

    错误映射到源文件

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
+    devtool:"inline-source-map",
     plugins:[
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
